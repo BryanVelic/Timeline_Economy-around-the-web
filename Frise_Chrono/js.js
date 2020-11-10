ca0b09f92ecc998980c213c3ca639dbf300e113d
Bryan <velicbryan@gmail.com>
2020-11-10 16:41:24 +0100

    Séparation code modal du reste

--- Frise_Chrono/js.js
+++
@@ -1,5 +0,0 @@
-function iframeclick() {
-document.getElementById("theiframe").contentWindow.document.body.onclick = function() {
-        document.getElementById("theiframe").contentWindow.location.reload();
-    }
-}
